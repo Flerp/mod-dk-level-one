9149c8c905e5671af9e07d15535d9723e8d8fc3a
Flerp <Halofreak411@gmail.com>
2025-11-08 08:05:38 -0800

    Adding level requirements to DK gear

--- sql/world/Skip_DK_Script.sql
+++ sql/world/Skip_DK_Script.sql
@@ -1,7 +1,6 @@
 -- no previous scriptname
 UPDATE `creature_template` SET `npcflag`=3, `ScriptName`='npc_ac_skip_lich' WHERE `entry`=25462;
 
-
 -- update quest XP rewards to 0 for DK starting quests
 UPDATE `quest_template` SET `RewardXPDifficulty`=0 WHERE `ID` IN (12593, 12619, 12842, 12848, 12636, 12641, 12657, 12678, 12679, 12680, 12687, 12698, 12701, 12706, 12716, 12719, 12720, 12722, 12724, 12725, 12727, 12733, 12751, 12754, 12755, 12756, 12757, 12779, 12801, 13165, 13166, 12750, 12749, 12748, 12747, 12746, 12745, 12744, 12743, 12742, 12739, 13188, 13189);
 
@@ -61,3 +60,6 @@ INSERT INTO `player_class_stats` VALUES
 (6,52,1185,0,102,73,93,29,40),
 (6,53,1241,0,104,73,95,29,41),
 (6,54,1299,0,106,73,97,29,41);
+
+-- DK Blue Gear set to level 61
+UPDATE `item_template` SET `RequiredLevel`=61 WHERE `entry` IN (38675, 38674, 38662, 38672, 38671, 38633, 38632, 38669, 38665, 38670, 38663, 38661, 38667, 38666, 38668, 39322, 38320, 38664, 38707);
